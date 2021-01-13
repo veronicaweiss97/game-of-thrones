20c3edf3c0cb16c5cc5c41f3e53f5b1c1330dc80
veronica <veronicaweiss77@gmail.com>
2021-01-13 20:42:19 +0100

    first commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-eclipse-fpiyocl43g"><div class="ldio-jydcjbj17a">
+        <div></div>
+        </div></div>
+
+    )
+}
+
+export default Spinner;
+
+
+
+
